6139539b1a2a667430dab708202f8be1ae18134b
Nguyen Ngoc Khanh <nguyenngockhanh.pbc@gmail.com>
2020-05-31 20:34:51 +0800

    modified:   .gitignore 	modified:   main.py 	new file:   received.mp3

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import pdb
 import subprocess
 from gtts import gTTS
 from audio2numpy import open_audio
@@ -15,6 +16,12 @@ def getClipboardData() -> str:
   data = p.stdout.read()
   return data.decode("utf-8")
 
+def setClipboardData(data: str):
+    p = subprocess.Popen(['xclip','-selection','clipboard'], stdin=subprocess.PIPE)
+    p.stdin.write(bytes(data, 'utf-8'))
+    p.stdin.close()
+    retcode = p.wait()
+
 def createAudioFile(text: str, lang= "en-AU", filename: str= "hello.mp3"):
   tts = gTTS(text= text, lang= lang)
   tts.save(filename)
@@ -30,21 +37,27 @@ def playSoundData(filename: str= "hello.mp3"):
   t.join()
 
 def play(lang= "en-AU", event = None):
-  print("play", event)
-  print("getting clipboard data")
-  text = getClipboardData()
-  print("calling text to speech api")
-  createAudioFile(text, lang)
-  print("start playing")
-  playSoundData()
-  print("done")
-  pass
+  try:
+    print("play", event)
+    print("getting clipboard data")
+    text = getClipboardData()
+    t = threading.Thread(target= playsound, args=("received.mp3",))
+    t.start()
+    #setClipboardData(text)
+    print("calling text to speech api:", text)
+    createAudioFile(text, lang)
+    print("start playing")
+    playSoundData()
+    print("done")
+    t.join()
+  except:
+    print("error")
 
 
 
 if __name__ == "__main__":
   hk = SystemHotkey()
-  hk.register(('alt', "shift", 'e'), callback= functools.partial(play, "en-AU"))
+  hk.register(('alt', "shift", 'e'), callback= functools.partial(play, "en"))
   hk.register(('alt', "shift", 'v'), callback= functools.partial(play, "vi"))
   sd.default.device = "pulse"
   print("ready")
